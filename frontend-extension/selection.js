3c22a22f32c3f56c5a41b956eeb0e902f802164a
Vincent Huang <vincenthuang75025@yahoo.com>
2021-08-16 14:01:12 -0800

    Re-add translation features with difficulty level

--- frontend-extension/selection.js
+++ frontend-extension/selection.js
@@ -1,19 +1,21 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method == "getSelection") {
       console.log([window.getSelection().toString(), request.difficulty]);
-        sendResponse({'data': window.getSelection().toString()});
+        // sendResponse({'data': window.getSelection().toString()});
+        // ^ placeholder response for development without api calls
 
-        // const url = 'https://p6era4zq99.execute-api.us-west-2.amazonaws.com/ServerlessHuggingFaceStack-testAF53AC38-8FVJp5hIbnMU';
-        // const options = {
-        // method: 'POST'}
+        const url = 'https://p6era4zq99.execute-api.us-west-2.amazonaws.com/ServerlessHuggingFaceStack-testAF53AC38-8FVJp5hIbnMU';
+        // replace ^ with your own url api. above url will not work. 
+        const options = {
+        method: 'POST'}
 
-        // fetch(url + '?' + 'difficulty=5&text=' + window.getSelection().toString(), options)
-        // .then(function(response) { // response is a ReadableStream
-        //     return response.text();
-        //   }).then(function(data) {
-        //     console.log(data);  
-        //     sendResponse({'data': data});
-        //   });
+        fetch(url + '?' + 'difficulty=' + request.difficulty.toString() + '&text=' + window.getSelection().toString(), options)
+        .then(function(response) { // response is a ReadableStream. convert to string
+            return response.text();
+          }).then(function(data) {
+            console.log(data);  
+            sendResponse({'data': data});
+          });
 
     }
     else
